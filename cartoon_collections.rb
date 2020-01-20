267847c1593d277518110604e0731406b2085b2f
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2020-01-20 19:17:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,11 @@ end
 
 def find_the_cheese(array)
    cheese_types = ["cheddar", "gouda", "camembert"]
-   array.find { |i| i == "cheddar" || i == "gouda" || i = "camembert" }
+   snacks.find do |item|
+    cheese_types.include?(item)
+  end
 end
+
 
 
 
